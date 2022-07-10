10d703c92ed06c54760624e2a7a3d9a70f3b0262
manulangat <emmanuelthedeveloper@gmail.com>
2022-07-10 20:27:42 +0300

    .

--- vars/deployDone.groovy
+++ vars/deployDone.groovy
@@ -2,6 +2,6 @@
 
 def call (String imageName , Integer version ) { 
 
-    echo "Deploy done, you will be receiving email shortly"
+    echo "Deploy done, you will be receiving email shortly for branch $BRANCH_NAME"
     echo "Whoops, here we g $imageName $version"
 }
